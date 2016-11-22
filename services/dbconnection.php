4afbfc8b94c7d68abb36e1ec72dee614b679a7fb
Glendeezy <gglendale17@gmail.com>
2016-11-22 00:23:42 -0800

    new heroku cleardb configuration.

--- services/dbconnection.php
+++ services/dbconnection.php
@@ -1,9 +1,13 @@
 <?php
-
-  $server = "localhost";
-  $username = "root";
-  $password = "root";
-  $database = "vending_machine_app";
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  // $server = "localhost";
+  // $username = "root";
+  // $password = "root";
+  // $database = "vending_machine_app";
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $database = substr($url["path"], 1);
 
   $connection =  mysqli_connect($server, $username, $password, $database);
 
@@ -11,7 +15,7 @@
   if(!$connection) {
     die("Connection failed: " . mysqli_connect_error($connection));
   } else {
-    // echo "Database Connection Successful!";
+    echo "Database Connection Successful!";
   }
 
 
